b25456a0208e19fb1d40d3ef44bc16cb9543bd19
Josiah Swab <josiahjswab@gmail.com>
2019-12-19 14:23:26 -0800

    Init push

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,21 @@
 const path = require('path');
 
-module.exports = {};
+module.exports = {
+  mode: 'none',
+  // this line allows you to see pre-bundled in browser dev tools source tab under webpack://
+  devtool: 'source-map',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    }]
+  }
+};
